07591487901e4e38d7822b2a106e065426eb8ac5
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-01-18 17:37:11 +0530

    json file made

---
+++ python/user.py
@@ -0,0 +1,51 @@
+import ozon3 as ooo
+import requests
+import pandas as pd
+import json
+
+o3 = ooo.Ozon3('3877cc8955afcd70e82ad4942dfb071c86801c7b')
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name"),
+        "latitude": response.get("latitude"),
+        "longitude": response.get("longitude")
+    }
+    return location_data
+information = get_location()
+latitude = information['latitude']
+longitude = information['longitude']
+final = o3.get_coordinate_air(latitude, longitude)
+#save final to json
+#convert final from dataframe to json
+
+print("AQI-",int(final.values[0][7]))
+##################################################################################################################3
+
+aqival = int(final.values[0][7])
+status = "helo"
+if(aqival>100):
+    status = "WARNING: AQI level is high and may be harmful for asthma affected individuals."
+
+if(aqival<100):
+    if(aqival>50):
+        status = "WARNING: Wearing a mask is advised"
+
+if(aqival<50):
+    status = "GREEN LIGHT: You stay at a place with healty AQI."
+
+final.to_json('file.json', orient = 'split', compression = 'infer', index = 'true')
+
+print(status)
+final['suggestion']= [status]
+final.to_json('file2.json', orient = 'split', compression = 'infer', index = 'true')
